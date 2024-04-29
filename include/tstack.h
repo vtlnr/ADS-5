d6cd2bdc2e97e886d457be8e03a9125c54afe5b4
vtlnr <149604564+vtlnr@users.noreply.github.com>
2024-04-29 15:31:47 +0300

    b107

--- include/tstack.h
+++ include/tstack.h
@@ -5,6 +5,28 @@
 template<typename T, int size>
 class TStack {
   // добавьте код стека
+  private:
+      T* arr;
+      int tIndex;
+  public:
+      TStack() {
+          arr = new T[size];
+          tIndex = -1;
+      }
+      bool isEmpty() {
+          return tIndex == -1;
+      }
+      void push(T value) {
+          arr[++tIndex] = value;
+      }
+      T& pop() {
+          return arr[tIndex--];
+      }
+      bool isFull() const {
+          return tIndex == size;
+      }
+      T& get() const {
+          return arr[tIndex]
+      }
 };
-
 #endif  // INCLUDE_TSTACK_H_
