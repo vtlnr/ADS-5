405af0e66522e67757ca4fd345f4ec7ab8394bfa
vtlnr <149604564+vtlnr@users.noreply.github.com>
2024-04-29 15:46:57 +0300

    b107

--- include/tstack.h
+++ include/tstack.h
@@ -5,6 +5,36 @@
 template<typename T, int size>
 class TStack {
   // добавьте код стека
+  public:
+    TStack() : top(-1) {
+     dt = new T[size];
+    }
+    void push(const T& item) {
+      if (tp <= size - 1)
+        dt[++tp] = item;
+      else
+        throw std::string("stack is full");
+    }
+    T pop() {
+      if (isEmpty())
+        throw std::string("stack is empty");
+      else
+        return dt[tp--];
+    }
+    ~TStack() {
+      delete[] dt;
+    }
+    bool isEmpty() const {
+      return tp == -1;
+    }
+    T get() const {
+      if (isEmpty())
+        throw std::string("stack is empty");
+      else
+        return dt[tp];
+    }
+  private:
+    T* dt;
+    int tp;
 };
-
 #endif  // INCLUDE_TSTACK_H_
